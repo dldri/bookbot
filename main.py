b54d8f894a174d5e50b1e98ad457767b5b71f853
dldri <diovian.liu@gmail.com>
2025-01-26 18:31:12 +0800

    new function get_chars_dict

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import string
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print (f"{num_words} words found in the document")
+    print(f"{num_words} words found in the document")
+
+    char_dict = get_chars_dict(text)
+    print(char_dict)
 
 def get_num_words(text):
     words = text.split()
@@ -11,5 +16,15 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
     
 main()
