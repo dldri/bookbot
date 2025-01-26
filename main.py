1f0251da4a60583d8695b706daba6b947bf9f3b1
dldri <diovian.liu@gmail.com>
2025-01-26 18:47:39 +0800

    code now prints document report

--- main.py
+++ main.py
@@ -4,10 +4,28 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
-
     char_dict = get_chars_dict(text)
-    print(char_dict)
+    char_list_dict = convert_to_list_of_dictionaries(char_dict)
+    char_list_dict.sort(reverse=True, key=sort_on)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document\n")
+
+    for dict in char_list_dict:
+        print(f"The '{dict["letter"]}' character was found {dict["count"]} times")
+
+    print("\n--- End of Report")
+
+def convert_to_list_of_dictionaries(dict):
+    char_list_dict = []
+    for char in dict:
+        if char in string.ascii_lowercase:
+            char_list_dict.append({"letter": char, "count": dict[char]})
+    return char_list_dict
+
+
+def sort_on(dict):
+    return dict["count"]
 
 def get_num_words(text):
     words = text.split()
